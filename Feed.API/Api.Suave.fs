ee8f35832e8131b5eaf2801a0570bfa66c0705df
Bohdan Makohin <bm@loopbycoop.com>
2020-06-27 02:54:15 +0300

    Add support of custom HTTP status codes for successful results

--- Feed.API/Api.Suave.fs
+++ Feed.API/Api.Suave.fs
@@ -6,26 +6,46 @@ open Suave.Operators
 open Suave.Successful
 open Suave.ServerErrors
 open Suave.RequestErrors
+open Response
+
 open FsToolkit.ErrorHandling
 
 open Api
 
-let mapAppResult asyncResult context =
-    asyncResult
-    |> AsyncResult.map (mapJson >> OK)
-    |> AsyncResult.mapError (function
-        | Application.DomainError msg -> (BAD_REQUEST, msg)
-        | Application.PersistenceError _ -> (INTERNAL_ERROR,  "Internal Server Error" ))
-    |> AsyncResult.mapError (fun (status, error) -> status (mapJson { Error = error }))
-    |> AsyncResult.foldResult id id
-    |> Async.RunSynchronously
-    <| context
+let mapFromJson<'T> (context : HttpContext) =
+    //do! obj.ReferenceEquals (dto, null) |> Result.requireFalse (DomainError "Request is empty")
+    parseJsonBytes<'T> context.request.rawForm
+
+let mapAppError appError =
+    match appError with
+        | Application.DomainError msg -> (HTTP_400, msg)
+        | Application.PersistenceError _ -> (HTTP_500,  "Internal Server Error" )
+
+let mapToApiError (httpCode, error) = (httpCode, { Error = error })
+
+let mapToHttpResponse (httpCode, content) =
+    content |> mapJson |> UTF8.bytes |> response httpCode
+
+let mapAppResult httpCode asyncResult = fun context -> async {
+    let! result = asyncResult
+
+    let httpStatus httpCode content = (httpCode, content)
+    
+    let response =
+        match result with
+        | Error appError -> appError |> mapAppError |> mapToApiError |> mapToHttpResponse
+        | Ok res -> res |> httpStatus httpCode |> mapToHttpResponse
+
+    return! response context
+}
+
+let (@) g f arg = f (g arg) arg
 
 let allHandlers = [
-    GET >=> path "/feeds" >=> mapAppResult (Application.getAllFeeds())
-    GET >=> pathScan "/feeds/%s" (mapAppResult << Application.getFeedByTitle)
-    POST >=> path "/feeds/" >=> (fun ctx -> mapAppResult (mapFromJson ctx |> Application.createFeed) ctx)
-    PUT >=> pathScan "/feeds/%d" (sprintf "Feed %d" >> NOT_IMPLEMENTED)
+    GET >=> path "/feeds" >=> (Application.getAllFeeds() |> mapAppResult HTTP_200)
+    POST >=> path "/feeds" >=> (mapFromJson >> Application.createFeed) @ mapAppResult HTTP_201
+    GET >=> pathScan "/feeds/%s" (Application.getFeedByTitle >> mapAppResult HTTP_200)
+    PUT >=> pathScan "/feeds/%s" (sprintf "Feed %s" >> asyncResult.Return >> mapAppResult HTTP_501)
 ]
 
 let run() =
--- Feed.API/Api.fs
+++ Feed.API/Api.fs
@@ -2,8 +2,6 @@ module Api
 
 open Newtonsoft.Json
 open System.Text
-open FsToolkit.ErrorHandling
-open Suave
 
 type Error = { Error : string }
 
@@ -13,7 +11,3 @@ let parseJsonBytes<'T> (bytes : byte array) =
     bytes
     |> Encoding.UTF8.GetString
     |> JsonConvert.DeserializeObject<'T>
-
-let mapFromJson<'T> (context : HttpContext) =
-    //do! obj.ReferenceEquals (dto, null) |> Result.requireFalse (DomainError "Request is empty")
-    parseJsonBytes<'T> context.request.rawForm
