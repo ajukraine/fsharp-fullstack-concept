2821a2a654ac6b99864e35c713d9d9eaea05f334
Bohdan Makohin <bm@loopbycoop.com>
2020-07-05 21:13:31 +0300

    Create basic property test for Feed

--- Feed.API/Tests.fs
+++ Feed.API/Tests.fs
@@ -1,10 +1,27 @@
 module Tests
 
 open Expecto
+open Domain
+open System
+
+let config = { FsCheckConfig.defaultConfig with maxTest = 10_000 }
+
+let ``feed PublishDate should be before ExpiryDate property`` = fun publishDate expiryDate ->
+    Feed.create "Some title" publishDate expiryDate |>
+        function
+        | Ok _ when publishDate >= expiryDate -> false
+        | _ -> true
 
 [<Tests>]
 let properties =
     testList "Sample" [
-        testProperty "None" <| ()
-        testProperty "Poggers" <| false
+        testPropertyWithConfig config "Feed PublishDate should be before ExpiryDate" <|
+            ``feed PublishDate should be before ExpiryDate property``
+        
+        testCase "PublishDate and ExpiryDate can't same" <| fun () ->
+            let now = DateTime.Now
+
+            "PublishDate and ExpiryDate can't same" |>
+                Expect.isTrue (``feed PublishDate should be before ExpiryDate property`` now now)
+                
     ]
